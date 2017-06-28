4bbec40df70078ce14b066f0bafe244fabf3a588
jslegend <hiroshikai@tutanota.com>
2017-06-28 21:30:16 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+#PostgreSQL & EAN Stack based Auth App
+_Template for a PostgreSQL & EAN stack based application_
+
+##Requirements
+* [NodeJS](https://nodejs.org/)
+* [ExpressJS](https://expressjs.com/)
+* [Angular4](https://angular.io/)
+* [PostgreSQL](https://www.postgresql.org/)
+
+ng build in Angular-Src
+
+nodemon in root dir
+
+
+npm install pg for Node Module for PostgreSQL
+
+
+table info
+
+name: users
+
+columns
+
+id: serial PK
+
+name: character varying 40
+
+email: character varying 40
+
+username: character varying 40
+
+password: character varying 100
+"# node-express-postgre-auth" 
---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const users = require('./routes/users');
+
+// Express App initialization
+const app = express();
+
+// Port Setting
+const port = process.env.PORT || 8080;
+
+// Cors Middleware
+app.use(cors());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+// Routing
+app.use('/users', users);
+
+app.get('/', function(req, res){
+    res.send('Invalid Endpoint');
+});
+
+// Entry Point
+app.get('*', function(req, res){
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+// Listening
+app.listen(port, function() {
+    console.log('Server started on port ' + port);
+});
